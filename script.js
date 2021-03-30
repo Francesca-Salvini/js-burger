79aad0deae7244019ef33508071f668d60bbce7e
Francesca Salvini <franci98sp@gmail.com>
2021-03-30 22:13:52 +0200

    se non si attribuisce  un nome al panino, un alert avverte di compilare l'apposita sezione

--- script.js
+++ script.js
@@ -10,33 +10,48 @@
 var calcolaButton = document.getElementById('calcola');
 calcolaButton.addEventListener( 'click', function (){
 
-var prezzoBasePanino = 10; //prezzo di partenza del panino
+    // se non do un nome al burger, non posso calcolare il prezzo finale
+    //
+    // se la lunghezza del nome del burger è pari a 0, un alert mi avverte
+    // di compilare l'apposita sezione
+    var nameYourBurger = document.getElementById('name-your-burger');
+    var nomeBurger = nameYourBurger.value;
+    console.log(nomeBurger);
+    if(nomeBurger.length == 0 ) {
+        alert('You have to name your burger! ');
+    } else {
+        var prezzoBasePanino = 10; //prezzo di partenza del panino
 
-var addonCheckboxes = document.getElementsByClassName('addon');
-// console.log(addonCheckboxes);
-
-for(var i = 0; i < addonCheckboxes.length; i++) {
-    var thisCheckbox = addonCheckboxes[i];
-    // console.log(thisCheckbox);
-
-    // console.log( i, thisCheckbox.checked);
-    // console.log( i, thisCheckbox.value);
-
-    if(thisCheckbox.checked == true ) {
-        //condidera i valori inseriti negli input 
-        var thisPrice = parseInt(thisCheckbox.value);   
-        // console.log( i, thisPrice);
-
-        prezzoBasePanino = prezzoBasePanino + thisPrice;
+        var addonCheckboxes = document.getElementsByClassName('addon');
+        // console.log(addonCheckboxes);
         
-    }
+        for(var i = 0; i < addonCheckboxes.length; i++) {
+            var thisCheckbox = addonCheckboxes[i];
+            // console.log(thisCheckbox);
+        
+            // console.log( i, thisCheckbox.checked);
+            // console.log( i, thisCheckbox.value);
+        
+            if(thisCheckbox.checked == true ) {
+                //condidera i valori inseriti negli input 
+                var thisPrice = parseInt(thisCheckbox.value);   
+                // console.log( i, thisPrice);
+        
+                prezzoBasePanino = prezzoBasePanino + thisPrice;
+                
+            }
+            
+        }
+        
+        console.log(prezzoBasePanino); //anteprima del prezzo finale
+        
+        //STAMPO prezzo a schermo (nel footer)
+        document.getElementById('your-price').innerHTML = prezzoBasePanino + '$';
     
-}
-
-console.log(prezzoBasePanino); //anteprima del prezzo finale
-
-//STAMPO prezzo a schermo (nel footer)
-document.getElementById('your-price').innerHTML = prezzoBasePanino + '$';
+    }
 
 });
+        
+        
+
 
